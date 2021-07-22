a9622b08b213324668f7cfa71573c00f81ee846a
Iulian Beleiu <iulian.beleiu@gmail.com>
2021-07-22 14:32:50 +0300

    update index async on post create

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
-use App\Model\Post;
 use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Elastica\Query\MultiMatch;
 use JoliCode\Elastically\Client;
+use JoliCode\Elastically\Messenger\IndexationRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
@@ -16,7 +18,7 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog")
      */
-    public function search(Client $client, Request $request): Response
+    public function search(Client $client, Request $request, PostRepository $postRepository): Response
     {
         $query = $request->query->get('q', '');
         $limit = $request->query->get('l', 10);
@@ -48,5 +50,27 @@ class BlogController extends AbstractController
         return $this->render('blog/index.html.twig', [
             'blogs' => $results,
         ]);
+    }
+
+    /**
+     * @Route("/blog/new")
+     */
+    public function newPost(
+        EntityManagerInterface $entityManager,
+        MessageBusInterface $bus
+    ) {
+        $post = new \App\Entity\Post();
+        $post->setTitle('testing');
+        $post->setAuthorName('testing');
+        $post->setSummary('testing');
+        $post->setPublishedAt(new \DateTimeImmutable());
+        $post->setSlug('testing');
+
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        $bus->dispatch(new IndexationRequest(\App\Model\Post::class, $post->getId()));
+
+        return new Response('created', Response::HTTP_CREATED);
     }
 }
--- src/Elasticsearch/DocumentExchanger.php
+++ src/Elasticsearch/DocumentExchanger.php
@@ -3,8 +3,29 @@
 
 namespace App\Elasticsearch;
 
+use App\Model\Post;
+use App\Repository\PostRepository;
+use Elastica\Document;
+use JoliCode\Elastically\Messenger\DocumentExchangerInterface;
 
-class DocumentExchanger
+class DocumentExchanger implements DocumentExchangerInterface
 {
+    private $postRepository;
 
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
+    public function fetchDocument(string $className, string $id): ?Document
+    {
+        if ($className === Post::class) {
+            $post = $this->postRepository->find($id);
+            if ($post) {
+                return new Document($id, $post->toModel());
+            }
+        }
+
+        return null;
+    }
 }
